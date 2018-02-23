3816e1af33d3edbdd32b37f2bb3beb080ed59297
R Adysurya A <ri7nz.labs@gmail.com>
2018-02-24 02:27:20 +0700

    Fix Image Default

--- part/header-image.php
+++ part/header-image.php
@@ -1,12 +1,9 @@
 <?php
 $img = sprintf("%s/img/custom_header.jpg", get_template_directory_uri());
-
-if( ! is_null(header_image())){
-    $img = header_image(); 
-}?>
-
+?>
 <div class="h-64 w-full bg-contain bg-center bg-no-repeat bg-grey-lighter"
-style="background-image: url('<?php echo $img ?>')"
+style="background-image: url('<?php if (header_image()) header_image(); else   echo $img; ?>')"
+
 >
 </div>
 
