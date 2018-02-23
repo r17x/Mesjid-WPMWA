c225b4cd8e45e028818bece457a7978e3f1c777f
R Adysurya A <ri7nz.labs@gmail.com>
2018-02-24 02:51:16 +0700

    Update IMage

--- part/header-image.php
+++ part/header-image.php
@@ -1,9 +1,4 @@
-<?php
-$img = sprintf("%s/img/custom_header.jpg", get_template_directory_uri());
-?>
 <div class="h-64 w-full bg-contain bg-center bg-no-repeat bg-grey-lighter"
-style="background-image: url('<?php if (header_image()) header_image(); else   echo $img; ?>')"
-
+style="background-image: url('<?php  header_image(); ?>')"
 >
 </div>
-
