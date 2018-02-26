fea478e194c2412aa8485f774e1cc47bdf72d0ca
R Adysurya A <ri7nz.labs@gmail.com>
2018-02-26 10:03:31 +0700

    Update SWJS.php

--- swjs.php
+++ swjs.php
@@ -1,17 +1,29 @@
 <?php 
-    $name = get_bloginfo('name');
+    $name      = get_bloginfo('name');
+    $themePath = get_template_directory_uri();
+
+    $list = [
+        '/',
+        '/style.css',
+        '/img/16x16.png',
+        '/img/apple-touch-icon-120x120.png',
+        '/img/32x32.png',
+        '/img/310x310.png', 
+    ];
+
+    $list = array_map( function($u) use($themePath){
+        if ($u === '/')
+            return $u;
+
+        return $themePath . $u; 
+    }, $list);
     
 ?>
 self.addEventListener('install', function(e) {
   e.waitUntil(
       caches.open('<?php echo $name?>').then(function(c){
           return c.addAll([
-              '/',
-              '/wp-content/themes/mesjid/style.css',
-              '/wp-content/wp-content/themes/mesjid/img/16x16.png',
-              '/wp-content/wp-content/themes/mesjid/img/apple-touch-icon-120x120.png',
-              '/wp-content/wp-content/themes/mesjid/img/32x32.png',
-              '/wp-content/wp-content/themes/mesjid/img/310x310.png'
+            <?php echo '"' . implode('","', $list) . '"' ?>
           ]) ; 
       })            
   );
