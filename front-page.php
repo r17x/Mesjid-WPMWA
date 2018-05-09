faf455bff2827c5afdf5130b5d1a3ad38799d7d4
R Adysurya A <ri7nz.labs@gmail.com>
2018-05-09 20:08:38 +0700

    update front page & center text

--- front-page.php
+++ front-page.php
@@ -1,16 +1,13 @@
 <?php get_header(); ?>
 <!-- Content -->
-<section id="contents" class="contents flex flex-col">
+<section id="contents" class="contents">
 <?php get_template_part('part/header-image'); ?>
-    <div class="container">
-    <section class="posts">
+    <div class="container page-archive">
+    <section id="post" class="posts col-34 px-4">
 <?php if( is_front_page() ): ?>
 <?php endif; ?>
 <div class="flex flex-wrap">
 <?php 
-# list post type want to load on front page
-# Get All Categories
-# Get Post By Category
 
 $categories = [
     'orderby' => 'name',
@@ -28,19 +25,20 @@ foreach( $categories as $category ):
     );
     $contents = new WP_Query($args);
 ?>
-<div class="col-2">
 <?php 
     echo sprintf("<h1 class='title'><a style='all:unset' href='%s'>%s</a></h1>",
         get_category_link($category->term_id), strtoupper($category->name));
     include('post-loop.php');
 ?>
-</div>
 <?php
     wp_reset_query(); 
 endforeach; # end foreach for categories
 ?>
 </div>
     </section>
+    <div class="col-14">
+    <?php get_sidebar(); ?>
+    </div>
 </div>
 </section>
 <!-- EndContent -->
